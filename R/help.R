58a6875786b62377062bfa24d25eeb3ff71bddd9
Robert Scharpf <rscharpf@Roberts-MacBook-Pro.local>
2016-03-11 19:13:52 -0500

    Cloned from svfilters

--- R/help.R
+++ R/help.R
@@ -1,7 +1,7 @@
 #' Germline filters for somatic structural variant analysis
 #'
 #' @docType package
-#' @name svfilters
+#' @name svfilters.hg18
 #' @importFrom BiocGenerics unlist
 #' @import GenomicRanges
 NULL
