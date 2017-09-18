695da6756aadc423a3c281f0b363eeca2034693f
Daniel Bruhm <danielbruhm@gmail.com>
2017-09-18 15:19:30 -0400

    Add code to generate gaps object

--- data-raw/binAssemblyGaps.R
+++ data-raw/binAssemblyGaps.R
@@ -54,3 +54,29 @@ file <- file.path(path, "wgEncodeCrgMapabilityAlign100mer.bw")
 map.bw <- import.bw(file)
 bins10kb$map <- mappability(map.bw, bins10kb)
 save(bins10kb, file="/dcl01/scharpf/data/svpackages/bins10kb.rda")
+
+genomeGaps <- function() {
+  library(BSgenome.Hsapiens.UCSC.hg18)
+  genome <- "hg18"
+  mySession <- browserSession()
+  genome(mySession) <- genome
+  gaps <- getTable(ucscTableQuery(mySession, track="gap"))
+  gaps.gr <- GRanges(gaps$chrom, IRanges(gaps$chromStart,
+                                         gaps$chromEnd),
+                     type=gaps$type)
+  gaps_hg18 <- gaps.gr
+  ## no gaps available in mitochondria
+  gaps_hg18 <- keepSeqlevels(gaps_hg18, paste0("chr", c(1:22, "X", "Y")), pruning.mode = "coarse")
+  seqinfo(gaps_hg18) <- seqinfo(Hsapiens)[seqlevels(gaps_hg18), ]
+  gaps_hg18 <- trim(gaps_hg18)
+  
+  # Reduce the two chrY centromere ranges
+  ycen <- gaps_hg18[which((gaps_hg18$type == "centromere") & (seqnames(gaps_hg18) == "chrY"))]
+  combined <- range(ycen)
+  combined$type <- "centromere"
+  gaps_hg18 <- gaps_hg18[-which((gaps_hg18$type == "centromere") & (seqnames(gaps_hg18) == "chrY"))]
+  gaps_hg18 <- c(gaps_hg18, combined)
+  gaps_hg18 <- sort(gaps_hg18)
+  save(gaps_hg18, file="/Users/dbruhm/Desktop/cancer-genomics/svfilters.hg18/data/gaps.rda")
+}
+
